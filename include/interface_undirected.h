df76c1c6392e5a1d8bd9233ed79d6e68fcc47257
Henrique-Brito <henriquejbrito@gmail.com>
2018-11-20 23:10:15 -0200

    Nova formar de construir

--- include/interface_undirected.h
+++ include/interface_undirected.h
@@ -6,23 +6,13 @@
 class Undirected_IF : public Graph_IF{
 public:
   Undirected_IF(int n);
-  ~Undirected_IF();
-  virtual void add_edge(int bg, int en);
-  virtual void add_edge(int bg, int en, int w);
-  virtual void remove_edge(int bg, int en);
+  virtual ~Undirected_IF();
   virtual int check_degree(int v);
-  virtual int connected() = 0;
-  virtual int has_cycle() = 0;
-  virtual int bipartite() = 0;
-  virtual int get_component(int v) = 0;
-  virtual int reflexive() = 0;
-  virtual int irreflexive() = 0;
-  virtual int symmetric() = 0;
-  virtual int antissymetric() = 0;
-  virtual int assymetric() = 0;
-  virtual int transitive() = 0;
-  virtual int euler_graph() = 0;
-  virtual int subgraph(Graph_IF &g) = 0;
+  virtual int connected();
+  virtual int has_cycle();
+  virtual int bipartite();
+  virtual int get_component(int v);
+  virtual int euler_graph();
 };
 
 #endif
