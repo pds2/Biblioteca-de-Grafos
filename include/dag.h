3f2b0fa9873f8d3a5aedf06dc88c55d5112daaef
Henrique-Brito <henriquejbrito@gmail.com>
2018-11-20 23:07:52 -0200

    Atualizando

--- include/dag.h
+++ include/dag.h
@@ -6,20 +6,14 @@
 class DAG : public Directed_IF{
 public:
   DAG(int n);
+  DAG(int n, Edges e);
   ~DAG();
+  virtual void add_edge(int bg, int en);
+  virtual void add_edge(int bg, int en, int w);
   virtual int *topological_order();
   virtual int connected();
   virtual Directed_IF *get_compressed_graph();
   virtual int bipartite();
-  virtual int get_component(int v);
-  virtual int reflexive();
-  virtual int irreflexive();
-  virtual int symmetric();
-  virtual int antissymetric();
-  virtual int assymetric();
-  virtual int transitive();
-  virtual int euler_graph();
-  virtual int subgraph(Graph_IF &g);
 };
 
 #endif
