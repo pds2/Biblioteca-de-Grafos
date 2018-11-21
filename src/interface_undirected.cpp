fa3f0ded057d9fbcfa44f8f5752508198d051eda
Henrique-Brito <henriquejbrito@gmail.com>
2018-11-20 23:12:25 -0200

    Atualizando

--- src/interface_undirected.cpp
+++ src/interface_undirected.cpp
@@ -1,1 +1,15 @@
 #include "interface_undirected.h"
+
+Undirected_IF::Undirected_IF(int n) : Graph_IF(n){}
+
+Undirected_IF::~Undirected_IF(){ }
+
+int Undirected_IF::euler_graph(){
+  for(int i = 1; i <= this->vertices; i++){
+      if(this->matrix[i][0]%2)
+          return 0;
+      if(this->matrix[0][i]%2)
+          return 0;
+  }
+  return 1;
+}
