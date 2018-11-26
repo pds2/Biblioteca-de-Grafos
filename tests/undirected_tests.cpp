13022a4b926efb47fb51980c9edddaeec2ddceb3
laura-vianna <laura.lucciola@outlook.com>
2018-11-26 22:33:02 +0000

    Adds tests for constructor and destructor

--- tests/undirected_tests.cpp
+++ tests/undirected_tests.cpp
@@ -1,1 +1,13 @@
 #include "undirected_graph.h"
+#include "doctest.h"
+
+
+
+TEST_CASE("TESTE - Construtor"){
+    CHECK_NOTHROW(Undirected u(1));
+}
+
+TEST_CASE("TESTE - Destrutor"){
+    Undirected *u = new Undirected(1);
+    CHECK_NOTHROW(delete u);
+}
