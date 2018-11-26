b01b22db356378b90b6c070c4650cc15dc223c11
Henrique-Brito <henriquejbrito@gmail.com>
2018-11-26 14:07:28 -0200

    Consertando

--- src/edges.cpp
+++ src/edges.cpp
@@ -13,8 +13,12 @@ const std::pair<int,std::pair<int,int> >& Edges::operator [](int idx){
 }
 
 void Edges::insert(int begin, int end, int weight){
-  if( begin > 1000 || end > 1000 || begin < 0 || end < 0 ){
+  if( begin > 1000 || end > 1000 || begin <= 0 || end <= 0 ){
     throw std::overflow_error("Aresta invalida");
   }
   v.push_back(std::make_pair(begin, std::make_pair(end, weight)));
+}
+
+void Edges::insert(int begin, int end){
+  insert(begin, end, 1);
 }
