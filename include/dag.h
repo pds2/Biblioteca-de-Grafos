eb3670fc6e7bba7f144d31b04c980050edb1f538
Henrique-Brito <henriquejbrito@gmail.com>
2018-11-26 20:40:39 -0200

    Consertando

--- include/dag.h
+++ include/dag.h
@@ -4,12 +4,14 @@
 #include "interface_directed.h"
 
 class DAG : public Directed_IF{
+private:
+  void add_edge_useful(int head_vertex, int tail_vertex, int w);
 public:
   DAG(int n);
   DAG(int n, Edges e);
   ~DAG();
-  virtual void add_edge(int bg, int en);
-  virtual void add_edge(int bg, int en, int w);
+  virtual void add_edge(int head_vertex, int tail_vertex);
+  virtual void add_edge(int head_vertex, int tail_vertex, int w);
   virtual int find_distance(int u, int v) override;
   virtual int *find_distance(int v) override;
 };
--- src/dag.cpp
+++ src/dag.cpp
@@ -3,32 +3,35 @@
 DAG::DAG(int n) : Directed_IF(n){}
 DAG::DAG(int n, Edges e): Directed_IF(n){
   for( int i=0; i<e.get_size(); i++ ){
-    add_edge(e[i].first, e[i].second.first, e[i].second.second);
+    add_edge_useful(e[i].first, e[i].second.first, e[i].second.second);
   }
 }
 DAG::~DAG(){ }
+void DAG::add_edge_useful(int head_vertex, int tail_vertex, int w){
+  if(head_vertex < MIN_GRAPH_SIZE || head_vertex > this->order())
+      throw std::overflow_error("Posição inicial para a aresta inválida");
+  if(tail_vertex < MIN_GRAPH_SIZE || tail_vertex > this->order())
+      throw std::overflow_error("Posição final para a aresta inválida");
+
+  if(!this->matrix[head_vertex][tail_vertex]){
+      this->matrix[0][0]++;
+      this->matrix[head_vertex][0]++;
+      this->matrix[0][tail_vertex]++;
+      this->matrix[0][head_vertex]++;
+      this->matrix[tail_vertex][0]++;
+  }
+  if(this->matrix[head_vertex][tail_vertex] >= 0){
+      if(w < 0)
+          this->has_negative_weight++;
+  }
+  this->matrix[head_vertex][tail_vertex] = w;
+  this->matrix[tail_vertex][head_vertex] = w;
+}
 void DAG::add_edge(int head_vertex, int tail_vertex){
   add_edge(head_vertex, tail_vertex, DEFAULT_EDGE_WEIGHT);
 }
 void DAG::add_edge(int head_vertex, int tail_vertex, int w){
-    if(head_vertex < MIN_GRAPH_SIZE || head_vertex > this->order())
-        throw std::overflow_error("Posição inicial para a aresta inválida");
-    if(tail_vertex < MIN_GRAPH_SIZE || tail_vertex > this->order())
-        throw std::overflow_error("Posição final para a aresta inválida");
-
-    if(!this->matrix[head_vertex][tail_vertex]){
-        this->matrix[0][0]++;
-        this->matrix[head_vertex][0]++;
-        this->matrix[0][tail_vertex]++;
-        this->matrix[0][head_vertex]++;
-        this->matrix[tail_vertex][0]++;
-    }
-    if(this->matrix[head_vertex][tail_vertex] >= 0){
-        if(w < 0)
-            this->has_negative_weight++;
-    }
-    this->matrix[head_vertex][tail_vertex] = w;
-    this->matrix[tail_vertex][head_vertex] = w;
+    add_edge_useful(head_vertex, tail_vertex, w);
     if( has_cycle() ){
       throw std::invalid_argument("Aresta inserida gera ciclo");
     }
